d51fc06d635302b96127ec6cfaf86ca56077b976
gajanandh <ghaligoudar@gmail.com>
2021-06-10 13:52:41 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React ,{useState,FC,FormEvent,ChangeEvent}from 'react';
+import QRcode from 'qrcode.react'
+import instance from './api/api'
+import {makeStyles} from '@material-ui/core/styles';
+import {TextField,Button} from '@material-ui/core';
 import './App.css';
 
-function App() {
+
+
+const useStyles =  makeStyles({
+  root:{
+    background:'blue'
+  },
+  shorturl:{
+    display:"block",
+    margin:20
+  }
+})
+const App:FC =()=> {
+
+  const [url,setUrl] = useState<string>("");
+  const [shorturl,setShorturl] = useState<string>('')
+  const classes = useStyles()
+
+  const handleSubmit= (e: FormEvent<HTMLFormElement>):void =>{
+    console.log(url)
+    e.preventDefault()
+    instance.post('/',{full:url})
+    .then(res=>{
+      setShorturl('http://localhost:8000/api/'+res.data.short)
+      console.log(res)
+    })
+    .catch(err=>console.log(err))
+    setUrl("")
+  }
+
+  const handleChange =(e:ChangeEvent<HTMLInputElement>):void=>{
+  setUrl(e.target.value)
+  // console.log(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form onSubmit={handleSubmit}>
+        <TextField
+        autoFocus
+        onChange = {handleChange}
+        color="secondary"
+        required
+        id = "urlinput"
+        label='URL'
+        placeholder="https://xyz.com"
+        value = {url}
+        />
+        <Button
+          type = "submit"
+          size="small"
+          variant = "contained"
+          color = "primary"
         >
-          Learn React
-        </a>
-      </header>
+          Generate
+        </Button>
+
+      </form>
+      <div className={classes.shorturl} id="shorturl"><a href={shorturl}>{shorturl}</a></div>
+      {shorturl?(<QRcode value={shorturl} />):null}
+      
     </div>
   );
 }
